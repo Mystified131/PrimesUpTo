93e139150ebb624fc8ae083124ff6a2a4198e7e2
mystified131 <mystifiedthomas@gmail.com>
2019-02-11 14:05:24 -0600

    Initial Commit.

---
+++ MVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Models;
+using MVCApplication.ViewModels;
+
+namespace MVCApplication.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            IndexViewModel indexViewModel = new IndexViewModel();
+
+            return View(indexViewModel);
+        }
+
+        public IActionResult Error()
+        {
+
+            return View();
+        }
+
+        public IActionResult Result()
+        {
+            ResultViewModel resultViewModel = new ResultViewModel();
+
+            resultViewModel.Error = "To test a new number, please return to the 'Add' page.";
+
+            return View(resultViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Result(ResultViewModel resultViewModel)
+
+        {
+            if (ModelState.IsValid)
+             
+
+            {
+
+               resultViewModel.State = true;
+               List<int> Answer = new List<int>();
+
+                for (int j = 0; j <= resultViewModel.Numin; j++){
+
+                    bool Ans = true;
+
+                if ((j <= 1) || (j == 2) || (j % 2 == 0))
+                {
+
+                    Ans = false;
+
+                }
+
+                var boundary = (int)Math.Floor(Math.Sqrt(resultViewModel.Numin));
+
+                for (int i = 3; i <= boundary; i += 2)
+                    if (j % i == 0)
+                    {
+                            Ans = false;
+                    }
+
+                if(Ans == true)
+                    {
+                       Answer.Add(j);
+                    
+                    }
+
+                }
+
+                resultViewModel.Answer = Answer;
+                return View(resultViewModel);
+            }
+
+
+            return Redirect("/Home/Error");
+
+        }
+
+    }
+
+}
+
---
+++ MVCApplication/ViewModels/ResultViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApplication.ViewModels
+{
+
+    public class ResultViewModel
+    {
+        [Required]
+        public int Numin { get; set; }
+        public bool State { get; set; }
+        public List<int> Answer { get; set; }
+        public String Error { get; set; }
+    }
+}
+
+
